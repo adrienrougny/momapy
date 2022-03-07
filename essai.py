5281a59cd51d959d5db41cdde773e8c433cb048f
Adrien Rougny <adrienrougny@gmail.com>
2022-03-07 18:24:26 +0100

    mod essai.py

--- essai.py
+++ essai.py
@@ -17,41 +17,29 @@ HEIGHT = 400
 RectangleBuilder = get_or_make_builder_cls(Rectangle)
 ArrowBuilder = get_or_make_builder_cls(Arrow)
 
-l = LayoutBuilder()
-r1 = RectangleBuilder(position=Point(100, 50), width=100, height=50, fill=colors.dark_magenta)
-# r1_label = NodeLayoutElementLabel(text="ABC", position=r1.position, width=r1.width, height=r1.height)
-# r1.label = r1_label
-
-r2 = RectangleBuilder(width=20, height=10, fill=colors.crimson)
-set_position_at(r2, r1.north(), anchor="south")
-
-r3 = RectangleBuilder(position=Point(300, 100), width=100, height=50, fill=colors.yellow_green)
-# r3_label = NodeLayoutElementLabel(text="DEF", position=r3.position, width=r3.width, height=r3.height)
-# r3.label = r3_label
+l = LayoutBuilder(fill=colors.light_salmon, stroke=colors.green_yellow, stroke_width=3)
+r1 = RectangleBuilder(position=Point(100, 100), width=100, height=50, fill=colors.blue)
+for i in range(0, 360, 10):
+    r = RectangleBuilder(position=r1.angle(i), width=5, height=5, stroke=colors.red)
+    r1.add_element(r)
 
 
+r3 = RectangleBuilder(position=Point(300, 300), width=100, height=50, fill=colors.yellow_green)
 
-r1.add_element(r2)
+
 
 a = ArrowBuilder()
-a.source = r1
-a.target = r3
+a.source = LayoutElementReference(r1)
+a.target = LayoutElementReference(r3)
 a.points.append(r1.border(r3.center()))
 a.points.append(r3.border(r1.center()))
-#
-# r4 = RectangleBuilder(width=20, height=10, fill=colors.sky_blue)
-# set_position_at_fraction_of(r4, a, 0.5, anchor="south")
-# a.add_element(r4)
-
+r4 = RectangleBuilder(width=20, height=10, fill=colors.sky_blue)
+set_position_at_fraction_of(r4, a, 0.5, anchor="south")
+a.add_element(r4)
 
 l.add_element(a)
-l.fill = colors.wheat
-l.stroke_width = 10
-l.stroke = colors.chartreuse
-
-# bbox = fit(l.flatten_group_layout_elements() + [Point(0, 0)], xsep=40, ysep=40)
-# l.width = bbox.width
-# l.height = bbox.height
+l.add_element(r1)
+l.add_element(r3)
 
 l.width = WIDTH
 l.height = HEIGHT
@@ -61,7 +49,7 @@ renderer = CairoRenderer(surface=surface, width=l.width, height=l.height)
 
 renderer.render_layout_element(l)
 
-for angle in range(20, 180, 20):
+for angle in range(20, 360, 20):
     print(angle)
     a.transform = (rotate(angle / 360 * 2 * 3.14159),)
     renderer.render_layout_element(a)
