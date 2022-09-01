dc6bc20a629f182daabcb1b7ec3fde71f367d157
Adrien Rougny <adrienrougny@gmail.com>
2022-09-01 19:06:48 +0200

    bug in set_compartments_to_fit_content

--- momapy/sbgn/utils.py
+++ momapy/sbgn/utils.py
@@ -3,10 +3,11 @@ import collections
 import momapy.positioning
 
 def set_compartments_to_fit_content(map_builder, xsep=0, ysep=0):
-    compartment_entity_pools_mapping = collections.defaultdict(set)
+    compartment_entity_pools_mapping = collections.defaultdict(list)
     model = map_builder.model
     for entity_pool in model.entity_pools:
-        if entity_pool.compartment is not None:
+        compartment = entity_pool.compartment
+        if compartment is not None:
             compartment_entity_pools_mapping[compartment].append(entity_pool)
     for compartment in compartment_entity_pools_mapping:
         compartment_layout = map_builder.model_layout_mapping[compartment]
