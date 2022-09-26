606f54ce37cf8c499f6139f80d8c3c95afd3aee7
Adrien Rougny <adrienrougny@gmail.com>
2022-09-26 18:32:02 +0200

    fixed tidy; set_arcs_to_borders does not do anything if no intersection is found

--- momapy/sbgn/utils.py
+++ momapy/sbgn/utils.py
@@ -3,6 +3,7 @@ import collections
 import momapy.positioning
 import momapy.builder
 
+
 def set_compartments_to_fit_content(map_builder, xsep=0, ysep=0):
     compartment_entity_pools_mapping = collections.defaultdict(list)
     model = map_builder.model
@@ -11,50 +12,54 @@ def set_compartments_to_fit_content(map_builder, xsep=0, ysep=0):
         if compartment is not None:
             compartment_entity_pools_mapping[compartment].append(entity_pool)
     for compartment in compartment_entity_pools_mapping:
-        for compartment_layout in map_builder.model_layout_mapping[
-                compartment]:
+        for compartment_layout in map_builder.model_layout_mapping[compartment]:
             elements = []
             for entity_pool in compartment_entity_pools_mapping[compartment]:
                 elements += map_builder.model_layout_mapping[entity_pool]
-            momapy.positioning.set_fit(
-                compartment_layout, elements, xsep, ysep)
+            momapy.positioning.set_fit(compartment_layout, elements, xsep, ysep)
             if compartment_layout.label is not None:
                 compartment_layout.label.position = compartment_layout.position
                 compartment_layout.label.width = compartment_layout.width
                 compartment_layout.label.height = compartment_layout.height
 
+
 def set_complexes_to_fit_content(map_builder, xsep=0, ysep=0):
     for entity_pool in map_builder.model.entity_pools:
-        if isinstance(entity_pool, momapy.builder.get_or_make_builder_cls(
-                momapy.sbgn.pd.Complex)):
-            for complex_layout in map_builder.model_layout_mapping[
-                    entity_pool]:
+        if isinstance(
+            entity_pool,
+            momapy.builder.get_or_make_builder_cls(momapy.sbgn.pd.Complex),
+        ):
+            for complex_layout in map_builder.model_layout_mapping[entity_pool]:
                 elements = []
                 for subunit in entity_pool.subunits:
-                    subunit_layouts = map_builder.model_layout_mapping[
-                        subunit]
+                    subunit_layouts = map_builder.model_layout_mapping[subunit]
                     for subunit_layout in subunit_layouts:
                         if subunit_layout in complex_layout.layout_elements:
                             elements.append(subunit_layout)
                 if len(elements) > 0:
                     momapy.positioning.set_fit(
-                        complex_layout, elements, xsep, ysep)
+                        complex_layout, elements, xsep, ysep
+                    )
                     if complex_layout.label is not None:
                         complex_layout.label.position = complex_layout.position
                         complex_layout.label.width = complex_layout.width
                         complex_layout.label.height = complex_layout.height
 
+
 def set_nodes_to_fit_labels(map_builder, xsep=0, ysep=0):
     for layout_element in map_builder.layout.flatten():
-        if (isinstance(layout_element, momapy.builder.NodeLayoutElementBuilder)
-                and layout_element.label is not None
+        if (
+            isinstance(layout_element, momapy.builder.NodeLayoutElementBuilder)
+            and layout_element.label is not None
         ):
-            position, width, height = momapy.positioning.fit([
-                layout_element.label.ink_bbox()], xsep, ysep)
+            position, width, height = momapy.positioning.fit(
+                [layout_element.label.ink_bbox()], xsep, ysep
+            )
             if width > layout_element.width:
                 layout_element.width = width
             if height > layout_element.height:
                 layout_element.height = height
+
 
 def set_arcs_to_borders(map_builder):
     for layout_element in map_builder.layout.flatten():
@@ -67,28 +72,41 @@ def set_arcs_to_borders(map_builder):
                 target = target.layout_element
             if source is not None or target is not None:
                 for main, index, increment, other in [
-                        (source, 0, 1, target), (target, -1, -1, source)]:
+                    (source, 0, 1, target),
+                    (target, -1, -1, source),
+                ]:
                     if main is not None:
                         if len(layout_element.points) > 2:
                             reference_point = layout_element.points[
-                                index + increment]
+                                index + increment
+                            ]
                         elif other is not None:
-                            if (hasattr(other, "base_left_connector") and not
-                                isinstance(layout_element, (
-                                momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.ModulationLayout),
-                                momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.StimulationLayout),
-                                momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.InhibitionLayout),
-                                momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.NecessaryStimulationLayout),
-                                momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.CatalysisLayout)
-                                ))
+                            if hasattr(
+                                other, "base_left_connector"
+                            ) and not isinstance(
+                                layout_element,
+                                (
+                                    momapy.builder.get_or_make_builder_cls(
+                                        momapy.sbgn.pd.ModulationLayout
+                                    ),
+                                    momapy.builder.get_or_make_builder_cls(
+                                        momapy.sbgn.pd.StimulationLayout
+                                    ),
+                                    momapy.builder.get_or_make_builder_cls(
+                                        momapy.sbgn.pd.InhibitionLayout
+                                    ),
+                                    momapy.builder.get_or_make_builder_cls(
+                                        momapy.sbgn.pd.NecessaryStimulationLayout
+                                    ),
+                                    momapy.builder.get_or_make_builder_cls(
+                                        momapy.sbgn.pd.CatalysisLayout
+                                    ),
+                                ),
                             ):
-                                if other.direction == \
-                                        momapy.core.Direction.HORIZONTAL:
+                                if (
+                                    other.direction
+                                    == momapy.core.Direction.HORIZONTAL
+                                ):
                                     if main.center().x < other.center().x:
                                         reference_point = other.west()
                                     else:
@@ -100,25 +118,36 @@ def set_arcs_to_borders(map_builder):
                                         reference_point = other.south()
                             else:
                                 reference_point = other.center()
-                        elif len(layout_element.points) > 1:
+                        elif len(layout_element.points) >= 2:
                             reference_point = layout_elements.points[
-                                index - increment]
-                        if (hasattr(main, "base_left_connector") and not
-                                isinstance(layout_element, (
+                                index - increment
+                            ]
+                        if hasattr(
+                            main, "base_left_connector"
+                        ) and not isinstance(
+                            layout_element,
+                            (
                                 momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.ModulationLayout),
+                                    momapy.sbgn.pd.ModulationLayout
+                                ),
                                 momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.StimulationLayout),
+                                    momapy.sbgn.pd.StimulationLayout
+                                ),
                                 momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.InhibitionLayout),
+                                    momapy.sbgn.pd.InhibitionLayout
+                                ),
                                 momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.NecessaryStimulationLayout),
+                                    momapy.sbgn.pd.NecessaryStimulationLayout
+                                ),
                                 momapy.builder.get_or_make_builder_cls(
-                                    momapy.sbgn.pd.CatalysisLayout)
-                                ))
+                                    momapy.sbgn.pd.CatalysisLayout
+                                ),
+                            ),
                         ):
-                            if main.direction == \
-                                    momapy.core.Direction.HORIZONTAL:
+                            if (
+                                main.direction
+                                == momapy.core.Direction.HORIZONTAL
+                            ):
                                 if reference_point.x < main.center().x:
                                     point = main.west()
                                 else:
@@ -130,22 +159,25 @@ def set_arcs_to_borders(map_builder):
                                     point = main.south()
                         else:
                             point = main.border(reference_point)
-                        layout_element.points[index] = point
+                        if point is not None:
+                            layout_element.points[index] = point
+
 
 def set_auxilliary_units_to_borders(map_builder):
-
     def _rec_set_auxilliary_units_to_borders(layout_element):
         for child in layout_element.children():
-            if (isinstance(
-                    child,
-                    (
-                        momapy.builder.get_or_make_builder_cls(
-                            momapy.sbgn.pd.StateVariableLayout),
-                        momapy.builder.get_or_make_builder_cls(
-                            momapy.sbgn.pd.UnitOfInformationLayout)
-                    )
-                ) and isinstance(
-                    layout_element, momapy.builder.NodeLayoutElementBuilder)
+            if isinstance(
+                child,
+                (
+                    momapy.builder.get_or_make_builder_cls(
+                        momapy.sbgn.pd.StateVariableLayout
+                    ),
+                    momapy.builder.get_or_make_builder_cls(
+                        momapy.sbgn.pd.UnitOfInformationLayout
+                    ),
+                ),
+            ) and isinstance(
+                layout_element, momapy.builder.NodeLayoutElementBuilder
             ):
                 position = layout_element.self_border(child.position)
                 child.position = position
@@ -155,26 +187,32 @@ def set_auxilliary_units_to_borders(map_builder):
 
     _rec_set_auxilliary_units_to_borders(map_builder.layout)
 
+
 def set_layout_to_fit_content(map_builder, xsep=0, ysep=0):
     momapy.positioning.set_fit(
-        map_builder.layout, map_builder.layout.layout_elements, xsep, ysep)
+        map_builder.layout, map_builder.layout.layout_elements, xsep, ysep
+    )
+
 
 def tidy(
-        map_builder,
-        nodes_xsep=3,
-        nodes_ysep=3,
-        complexes_xsep=15,
-        complexes_ysep=15,
-        compartments_xsep=15,
-        compartments_ysep=15,
-        layout_xsep=15,
-        layout_ysep=15
+    map_builder,
+    nodes_xsep=3,
+    nodes_ysep=3,
+    complexes_xsep=15,
+    complexes_ysep=15,
+    compartments_xsep=15,
+    compartments_ysep=15,
+    layout_xsep=15,
+    layout_ysep=15,
 ):
     set_nodes_to_fit_labels(map_builder, nodes_xsep, nodes_ysep)
     set_auxilliary_units_to_borders(map_builder)
     set_complexes_to_fit_content(map_builder, complexes_xsep, complexes_ysep)
+    set_nodes_to_fit_labels(map_builder, nodes_xsep, nodes_ysep)
     set_auxilliary_units_to_borders(map_builder)
     set_compartments_to_fit_content(
-        map_builder, compartments_xsep, compartments_ysep)
+        map_builder, compartments_xsep, compartments_ysep
+    )
+    set_nodes_to_fit_labels(map_builder, nodes_xsep, nodes_ysep)
     set_arcs_to_borders(map_builder)
     set_layout_to_fit_content(map_builder, layout_xsep, layout_ysep)
