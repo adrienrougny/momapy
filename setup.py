25e8816a3067b31c258c9b465f0ac1a158a91d79
Adrien Rougny <adrienrougny@gmail.com>
2022-10-25 18:41:07 +0200

    upadted setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
 
-setup(name = "momapy",
-    version = "1.0",
-    description = 'Molecular Maps Python library',
-    author = "Adrien Rougny",
-    author_email = "adrienrougny@gmail.com",
-    packages = ["momapy"],
-    install_requires = [
+setup(
+    name="momapy",
+    version="1.0",
+    description="Molecular Maps Python library",
+    author="Adrien Rougny",
+    author_email="adrienrougny@gmail.com",
+    packages=["momapy"],
+    install_requires=[
         "libsbgnpy",
         "frozendict",
         "pycairo",
-        "pygobject"
+        "pygobject",
+        "numpy",
+        "pyparsing",
     ],
 )
