f71cb96f4373caaa5f439ad158fd7bcfd5687b8a
Adrien Rougny <adrienrougny@gmail.com>
2023-06-27 17:33:56 +0200

    cleaned up

--- momapy/sbgn/io/make_sbgnml_parser.sh
+++ momapy/sbgn/io/make_sbgnml_parser.sh
@@ -1,4 +1,4 @@
 cp __init__.py __init__.py.save
-xsdata schema/sbgn-ml.xsd --debug --structure-style single-package --package _sbgnml_parser
+xsdata sbgnml_schemas/sbgn-ml.xsd --debug --structure-style single-package --package _sbgnml_parser
 mv __init__.py.save __init__.py
 mv sbgnml_parser.py _sbgnml_parser.py
