fc16aa4ade8f77c59a3ae1a7afe2da27320f5df6
Adrien Rougny <adrienrougny@gmail.com>
2023-09-25 19:33:51 +0200

    added rendering utility functions

--- momapy/utils.py
+++ momapy/utils.py
@@ -3,6 +3,9 @@ import collections.abc
 import dataclasses
 import colorama
 
+import momapy.rendering.core
+import momapy.rendering.skia
+
 
 def pretty_print(obj, max_depth=0, _depth=0, _indent=0):
     def _print_with_indent(s, indent):
@@ -52,3 +55,105 @@ def pretty_print(obj, max_depth=0, _depth=0, _indent=0):
 def print_classes(obj):
     for cls in type(obj).__mro__:
         print(cls)
+
+
+def render_nodes_on_grid(
+    output_file,
+    nodes,
+    n_cols,
+    width,
+    height,
+    x_margin=10.0,
+    y_margin=10.0,
+    format="pdf",
+    renderer="skia",
+):
+    max_x = x_margin * 2 + n_cols * width
+    n_rows = len(nodes) // n_cols + int(bool(len(nodes) % n_cols))
+    max_y = y_margin * 2 + n_rows * height
+    renderer = momapy.rendering.core.renderers[renderer].from_file(
+        output_file, max_x, max_y, format
+    )
+    renderer.begin_session()
+    for i, node in enumerate(nodes):
+        n_row = i // n_cols
+        n_col = i % n_cols
+        position = momapy.geometry.Point(
+            x_margin + width * (n_col + 1 / 2),
+            y_margin + height * (n_row + 1 / 2),
+        )
+        if not isinstance(node, momapy.core.NodeLayoutBuilder):
+            node = momapy.builder.builder_from_object(node)
+        tx = position.x - node.position.x
+        ty = position.y - node.position.y
+        translation = momapy.geometry.Translation(tx, ty)
+        if node.transform is None:
+            node.transform = momapy.core.TupleBuilder()
+        node.transform.append(translation)
+        node = momapy.builder.object_from_builder(node)
+        renderer.render_layout_element(node)
+    renderer.end_session()
+
+
+def render_nodes_testing(
+    output_file,
+    node_configs,
+    width,
+    height,
+    x_margin=10.0,
+    y_margin=10.0,
+    format="pdf",
+    renderer="skia",
+    node_width=60.0,
+    node_height=30.0,
+    border_stroke=None,
+    border_stroke_width=None,
+    border_fill=None,
+):
+    node_objs = []
+    for node_config in node_configs:
+        node_cls = node_config[0]
+        kwargs = node_config[1]
+        if "width" not in kwargs:
+            kwargs["width"] = node_width
+        if "height" not in kwargs:
+            kwargs["height"] = node_height
+        kwargs["position"] = momapy.geometry.Point(0, 0)
+        if "border_stroke" not in kwargs:
+            kwargs["border_stroke"] = border_stroke
+        if "border_stroke_width" not in kwargs:
+            kwargs["border_stroke_width"] = border_stroke_width
+        if "border_fill" not in kwargs:
+            kwargs["border_fill"] = border_fill
+        node_obj = node_cls(**kwargs)
+        node_objs.append(node_obj)
+        anchor_cross_points = []
+        for anchor_name in [
+            "north_west",
+            "north",
+            # "north_east",
+            # "east",
+            # "south_east",
+            # "south",
+            # "south_west",
+            # "west",
+        ]:
+            p = getattr(node_obj, anchor_name)()
+            cross_point = momapy.nodes.CrossPoint(
+                position=p, width=10.0, height=10.0, stroke=momapy.coloring.red
+            )
+            anchor_cross_points.append(cross_point)
+        kwargs["layout_elements"] = tuple(anchor_cross_points)
+        node_obj = node_cls(**kwargs)
+        node_objs.append(node_obj)
+    render_nodes_on_grid(
+        output_file=output_file,
+        nodes=node_objs,
+        n_cols=2,
+        width=width,
+        height=height,
+        x_margin=x_margin,
+        y_margin=y_margin,
+        format=format,
+        renderer=renderer,
+    )
