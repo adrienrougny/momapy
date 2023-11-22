461dee0422f7bd1372676b27dbc43e350e1d41ee
Adrien Rougny <adrienrougny@gmail.com>
2023-11-22 19:36:33 +0100

    added chdir for relative css @import

--- momapy/sbgn/styling/__init__.py
+++ momapy/sbgn/styling/__init__.py
@@ -1,6 +1,10 @@
 import pathlib
+import os
 
 import momapy.styling
+
+current_dir = os.getcwd()
+os.chdir(pathlib.Path(__file__).parent)
 
 default = momapy.styling.StyleSheet.from_file(
     pathlib.Path(__file__).with_name("./default.css")
@@ -20,3 +24,4 @@ newt = momapy.styling.StyleSheet.from_file(
 shadows = momapy.styling.StyleSheet.from_file(
     pathlib.Path(__file__).with_name("./shadows.css")
 )
+os.chdir(current_dir)  # ugly, to fix later
