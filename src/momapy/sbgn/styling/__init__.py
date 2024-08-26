c8746dc67569cf9987d4cb7fb86773e135256fc9
Adrien Rougny <adrienrougny@gmail.com>
2024-08-26 14:27:40 +0200

    fixed bug with file names

--- src/momapy/sbgn/styling/__init__.py
+++ src/momapy/sbgn/styling/__init__.py
@@ -7,18 +7,18 @@ current_dir = os.getcwd()
 os.chdir(pathlib.Path(__file__).parent)
 
 cs_default = momapy.styling.StyleSheet.from_file(
-    pathlib.Path(__file__).with_name("./cs_default.css")
+    pathlib.Path(__file__).with_name("cs_default.css")
 )
 cs_black_and_white = momapy.styling.StyleSheet.from_file(
-    pathlib.Path(__file__).with_name("./cs_black_and_white.css")
+    pathlib.Path(__file__).with_name("cs_black_and_white.css")
 )
 sbgned = momapy.styling.StyleSheet.from_file(
-    pathlib.Path(__file__).with_name("./sbgned.css")
+    pathlib.Path(__file__).with_name("sbgned.css")
 )
 newt = momapy.styling.StyleSheet.from_file(
-    pathlib.Path(__file__).with_name("./newt.css")
+    pathlib.Path(__file__).with_name("newt.css")
 )
 fs_shadows = momapy.styling.StyleSheet.from_file(
-    pathlib.Path(__file__).with_name("./fs_shadows.css")
+    pathlib.Path(__file__).with_name("fs_shadows.css")
 )
 os.chdir(current_dir)  # ugly, to fix later
