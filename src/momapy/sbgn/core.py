b08a8cb4614fd73d55ec2b8d192dd041c31cf072
Adrien Rougny <adrienrougny@gmail.com>
2024-10-30 15:47:24 +0100

    removed annotations and notes

--- src/momapy/sbgn/core.py
+++ src/momapy/sbgn/core.py
@@ -1,4 +1,3 @@
-import enum
 import abc
 import dataclasses
 import typing
@@ -7,56 +6,11 @@ import momapy.core
 import momapy.geometry
 
 
-class BiomodelQualifier(enum.Enum):
-    """Abstract class for http://biomodels.net/ qualifiers"""
-
-    pass
-
-
-class BQModel(BiomodelQualifier):
-    """Class for http://biomodels.net/model-qualifiers/ qualifiers"""
-
-    HAS_INSTANCE = "hasInstance"
-    IS = "is"
-    IS_DERIVED_FROM = "isDerivedFrom"
-    IS_DESCRIBED_BY = "isDescribedBy"
-    IS_INSTANCE_OF = "isInstanceOf"
-
-
-class BQBiol(BiomodelQualifier):
-    """Class for http://biomodels.net/biology-qualifiers/ qualifiers"""
-
-    ENCODES = "encodes"
-    HAS_PART = "hasPart"
-    HAS_PROPERTY = "hasProperty"
-    HAS_VERSION = "hasVersion"
-    IS = "is"
-    IS_DESCRIBED_BY = "isDescribedBy"
-    IS_ENCODED_BY = "isEncodedBy"
-    IS_HOMOLOG_TO = "isHomologTo"
-    IS_PART_OF = "isPartOf"
-    IS_PROPERTY_OF = "isPropertyOf"
-    IS_VERSION_OF = "isVersionOf"
-    OCCURS_IN = "occursIn"
-    HAS_TAXON = "hasTaxon"
-
-
-@dataclasses.dataclass(frozen=True, kw_only=True)
-class Annotation(momapy.core.ModelElement):
-    """Class for annotations"""
-
-    qualifier: BiomodelQualifier
-    resource: str
-
-
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SBGNModelElement(momapy.core.ModelElement):
     """Abstract class for SBGN model elements"""
 
-    notes: str | None = None
-    annotations: frozenset[Annotation] = dataclasses.field(
-        default_factory=frozenset, compare=False
-    )
+    pass
 
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
@@ -77,11 +31,6 @@ class SBGNRole(SBGNModelElement):
 class SBGNModel(momapy.core.Model):
     """Abstract class for SBGN models"""
 
-    notes: str | None = None
-    annotations: frozenset[Annotation] = dataclasses.field(
-        default_factory=frozenset, compare=False
-    )
-
 
 @dataclasses.dataclass(frozen=True, kw_only=True)
 class SBGNLayout(momapy.core.Layout):
@@ -98,10 +47,6 @@ class SBGNMap(momapy.core.Map):
 
     model: SBGNModel
     layout: SBGNLayout
-    notes: str | None = None
-    annotations: frozenset[Annotation] = dataclasses.field(
-        default_factory=frozenset, compare=False
-    )
 
 
 @dataclasses.dataclass(frozen=True)
