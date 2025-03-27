6a863e246bcac86fb47124f166135472041c06e8
Adrien Rougny <adrienrougny@gmail.com>
2025-03-27 14:48:09 +0100

    fixed bug outputing wrong map element types depending on mode

--- src/momapy/celldesigner/io/pickle.py
+++ src/momapy/celldesigner/io/pickle.py
@@ -34,6 +34,23 @@ class CellDesignerPickleReader(momapy.io.Reader):
         with_annotations=True,
         with_notes=True,
     ) -> momapy.io.ReaderResult:
+
+        def _del_key_from_mapping_by_classes(
+            mapping, include_classes=None, exclude_classes=None
+        ):
+            keys_to_delete = []
+            if include_classes is None:
+                include_classes = []
+            if exclude_classes is None:
+                exclude_classes = [object]
+            for key in mapping:
+                if isinstance(key, tuple(exclude_classes)) and not isinstance(
+                    key, tuple(include_classes)
+                ):
+                    keys_to_delete.append(key)
+            for key_to_delete in keys_to_delete:
+                del mapping[key_to_delete]
+
         with open(file_path, "rb") as f:
             reader_result = pickle.load(f)
         if not with_annotations:
@@ -43,15 +60,58 @@ class CellDesignerPickleReader(momapy.io.Reader):
         obj = reader_result.obj
         if return_type == "model":
             obj = obj.model
+            for mapping in [
+                reader_result.annotations,
+                reader_result.notes,
+                reader_result.ids,
+            ]:
+                if mapping is not None:
+                    _del_key_from_mapping_by_classes(
+                        mapping, include_classes=[momapy.core.ModelElement]
+                    )
         elif return_type == "layout":
             obj = obj.layout
+            for mapping in [
+                reader_result.annotations,
+                reader_result.notes,
+                reader_result.ids,
+            ]:
+                if mapping is not None:
+                    _del_key_from_mapping_by_classes(
+                        mapping, include_classes=[momapy.core.LayoutElement]
+                    )
         else:
             if not with_model or not with_layout:
                 map_builder = momapy.builder.builder_from_object(obj)
                 if not with_model:
                     map_builder.model = None
+                    for mapping in [
+                        reader_result.annotations,
+                        reader_result.notes,
+                        reader_result.ids,
+                    ]:
+                        if mapping is not None:
+                            _del_key_from_mapping_by_classes(
+                                mapping,
+                                exclude_classes=[
+                                    momapy.core.ModelLayout,
+                                ],
+                            )
+
                 if not with_layout:
                     map_builder.layout = None
+                    for mapping in [
+                        reader_result.annotations,
+                        reader_result.notes,
+                        reader_result.ids,
+                    ]:
+                        if mapping is not None:
+                            _del_key_from_mapping_by_classes(
+                                mapping,
+                                exclude_classes=[
+                                    momapy.core.LayoutElement,
+                                ],
+                            )
                 map_builder.layout_model_mapping = None
                 obj = momapy.builder.object_from_builder(map_builder)
         reader_result.obj = obj
