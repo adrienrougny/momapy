f65cc6daf9b3ee0cb00a93338d9bd56b16153bf4
Adrien Rougny <adrienrougny@gmail.com>
2025-12-16 16:12:50 +0100

    moved registering svg-native renderer to base of subpackage

--- src/momapy/rendering/__init__.py
+++ src/momapy/rendering/__init__.py
@@ -1,1 +1,11 @@
 """Subpackage for rendering"""
+
+import momapy.rendering.core
+import momapy.rendering.svg_native
+
+momapy.rendering.core.register_renderer(
+    "svg-native", momapy.rendering.svg_native.SVGNativeRenderer
+)
+momapy.rendering.core.register_renderer(
+    "svg-native-compat", momapy.rendering.svg_native.SVGNativeCompatRenderer
+)
